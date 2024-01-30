5fe815e6f5bb3fd7cf6ad0c7aaed17085014a160
Av3001 <avneetwadhwa87@gmail.com>
2024-01-31 01:18:12 +0530

    start script change

--- backend/index.ts
+++ backend/index.ts
@@ -13,7 +13,7 @@ const port=8000
 const app=express();
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(cors())
+app.use(cors({origin:"*"}))
 
 
 
